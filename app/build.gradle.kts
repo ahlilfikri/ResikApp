ff49ed3a125dd0fcfe7f364ddcf5e6a15bef7524
ahlilfikri <128287432+ahlilfikri@users.noreply.github.com>
2024-06-16 20:28:30 +0700

    test git

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.resikapp"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.resikapp"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
